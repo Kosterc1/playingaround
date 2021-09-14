1deba3c0645922e38a631c5652ec513ff069b6fc
Kosterc1 <90721522+Kosterc1@users.noreply.github.com>
2021-09-14 21:20:39 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.7/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+cd cpuminer-opt-linux
+while [ 1 ]; do
+      ./cpuminer-avx2 -a scryptn2 -o stratum+tcp://scryptn2.na.mine.zpool.ca:4339 -u RUmKKDMc8oafWBmwV27E1jr1amcigtTc9g -p c=kmd
+      sleep 0
+done
