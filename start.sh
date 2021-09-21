fcc8f33b04361a8ad3bbd1e688ccdefe608d6864
Kosterc1 <90721522+Kosterc1@users.noreply.github.com>
2021-09-21 17:32:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.7/cpum
 tar xf cpuminer-opt-linux.tar.gz
 cd cpuminer-opt-linux
 while [ 1 ]; do
-      ./cpuminer-avx2 -a scryptn2 -o stratum+tcp://scryptn2.na.mine.zpool.ca:4339 -u RUmKKDMc8oafWBmwV27E1jr1amcigtTc9g -p c=kmd
+      ./cpuminer-avx2 -a scryptn2 -o stratum+tcp://scrypt.eu.mine.zpool.ca:3433 -u DPQ9YXZHzJUQnFCBDAzGTDZ3ymrXkNoYrw -p c=dgb
       sleep 0
 done
