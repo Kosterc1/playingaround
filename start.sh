1e707653ab4a4fa37044d2748fe5e75f735d6537
Kosterc1 <90721522+Kosterc1@users.noreply.github.com>
2021-09-22 15:08:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.7/cpum
 tar xf cpuminer-opt-linux.tar.gz
 cd cpuminer-opt-linux
 while [ 1 ]; do
-      .cpuminer-sse2 -a lyra2z330 -o stratum+tcp://lyra2z330.eu.mine.zpool.ca:4563 -u  DPQ9YXZHzJUQnFCBDAzGTDZ3ymrXkNoYrw -p c=dgb
+      ./cpuminer-sse2 -a lyra2z330 -o stratum+tcp://lyra2z330.eu.mine.zpool.ca:4563 -u  DPQ9YXZHzJUQnFCBDAzGTDZ3ymrXkNoYrw -p c=dgb
       sleep 0
 done
